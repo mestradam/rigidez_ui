23b9b75d955f4ad7d51edcb81d3f7f72d7ac7d74
mestradam <mestradam@unal.edu.co>
2019-08-08 20:58:38 -0500

    First commit

---
+++ js/main.js
@@ -0,0 +1,51 @@
+const electron = require('electron');
+const {app, BrowserWindow} = electron;
+// const {Menu, globalShortcut} = electron; // to be used later
+// const path = require('path');
+// const url = require('url');
+
+// Keep a global reference to the main window variable
+let mainWindow;
+
+// -----------------------------------------------------------------------------
+// Main function to create the window.
+
+function createWindow () {
+
+  // Create the browser window.
+  mainWindow = new BrowserWindow({width: 1400, height: 900});
+
+  // Load the index.html file of the app.
+  mainWindow.loadFile('index.html');
+
+  // Open the DevTools.
+  // mainWindow.webContents.openDevTools()
+
+  // Emitted when the window is closed.
+  mainWindow.on('closed', () => {
+    mainWindow = null
+  });
+}
+
+// -----------------------------------------------------------------------------
+// Methods.
+
+// Create the window then the app is ready
+app.on('ready', createWindow );
+
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+  // On macOS it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (mainWindow === null) {
+    createWindow();
+  }
+})
---
+++ js/renderer.js
@@ -0,0 +1,166 @@
+//COLORS
+var Colors = {
+  node:0x359245,
+  floor:0x898989,
+  element:0x0a509e,
+  elementExtruded:0x70aedc,
+};
+
+// THREEJS RELATED VARIABLES
+
+var scene,
+    camera, fieldOfView, aspectRatio, nearPlane, farPlane,
+    renderer, container;
+
+//SCREEN & MOUSE VARIABLES
+
+var height, width, controls;
+
+//INIT THREE JS, SCREEN AND MOUSE EVENTS
+
+function createScene() {
+
+  height = window.innerHeight;
+  width = window.innerWidth;
+
+  scene = new THREE.Scene();
+  aspectRatio = width / height;
+  fieldOfView = 60;
+  nearPlane = 0.1;
+  farPlane = 10000;
+  camera = new THREE.PerspectiveCamera(
+    fieldOfView,
+    aspectRatio,
+    nearPlane,
+    farPlane
+    );
+  scene.fog = new THREE.Fog(0xf7d9aa, 100,950);
+  camera.position.x = 0;
+  camera.position.y = 10;
+  camera.position.z = 30;
+
+  renderer = new THREE.WebGLRenderer({ alpha: true, antialias: true });
+  renderer.setSize(width, height);
+  renderer.shadowMap.enabled = true;
+  container = document.getElementById('world');
+  container.appendChild(renderer.domElement);
+
+  controls = new THREE.OrbitControls(camera, container);
+  controls.target.set(0, 0, 0);
+  controls.update();
+
+  window.addEventListener('resize', handleWindowResize, false);
+}
+
+// HANDLE SCREEN EVENTS
+
+function handleWindowResize() {
+  height = window.innerHeight;
+  width = window.innerWidth;
+  renderer.setSize(width, height);
+  camera.aspect = width / height;
+  camera.updateProjectionMatrix();
+}
+
+
+// LIGHTS
+
+var hemisphereLight, light;
+
+function createLights() {
+
+  hemisphereLight = new THREE.HemisphereLight(0xffffff, 0x090909, 0.8);
+  light = new THREE.DirectionalLight(0xffffff, 0.9);
+  light.position.set(150, 350, 350);
+
+  scene.add(hemisphereLight);
+  scene.add(light);
+}
+
+// Render functions
+
+function loop(){
+  updateModel();
+  controls.update();
+  renderer.render(scene, camera);
+  requestAnimationFrame(loop);
+}
+
+function updateModel(){
+  // Put anything to update at every time frame. For example animations.
+  // airplane.propeller.rotation.x += 0.3;
+}
+
+function init(event){
+  createScene();
+  createLights();
+  createFloor(20, 12);
+  createMesh();
+  loop();
+}
+
+
+window.addEventListener('load', init, false);
+
+
+////////////////////////////////////////////////////////////////////////////////
+// 3D MODELS
+
+var floor, mesh;
+
+function createFloor(a, b){
+  var geom = new THREE.BoxGeometry(a, 0.1, b);
+  var mat = new THREE.MeshPhongMaterial({
+    color: Colors.floor,
+    transparent: true,
+    opacity: 0.6
+  });
+  floor = new THREE.Mesh(geom, mat);
+  floor.receiveShadow = true;
+  floor.position.y = -0.05;
+  scene.add(floor);
+}
+
+function createElement(ni, nj){
+  var mat = new THREE.LineBasicMaterial({
+    color: Colors.element,
+    linewidth: 3
+  });
+  var geom = new THREE.Geometry();
+  geom.vertices.push(ni);
+  geom.vertices.push(nj);
+  var element = new THREE.Line(geom, mat);
+  return element;
+}
+
+function createNode(ni){
+  var mat = new THREE.PointsMaterial({
+    color: Colors.node,
+    size: 0.5
+  });
+  var geom = new THREE.Geometry();
+  geom.vertices.push(ni);
+  var node = new THREE.Points(geom, mat);
+  return node;
+}
+
+function createMesh(){
+  mesh = new THREE.Object3D();
+}
+
+// User functions (to be used in chrome console)
+
+function node(coord){
+  var n = new THREE.Vector3(coord[0], coord[1], coord[2]);
+  var newNode = createNode(n);
+  mesh.add(newNode);
+  scene.add(mesh);
+}
+
+function elem(coord1, coord2){
+  var n1 = new THREE.Vector3(coord1[0], coord1[1], coord1[2]);
+  var n2 = new THREE.Vector3(coord2[0], coord2[1], coord2[2]);
+  var newElem = createElement(n1, n2);
+  mesh.add(newElem);
+  scene.add(mesh);
+}
